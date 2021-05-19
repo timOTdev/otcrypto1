00d8f21b6395d3fdb84778b5399dc31715be88c4
Tim H <timh1203@gmail.com>
2021-05-18 21:12:42 -0500

    Add footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,45 @@
-import styled from "styled-components";
-import Link from "next/link";
+import styled from 'styled-components';
+import SocialsNav from '../components_shared/SocialsNav';
+import SiteNav from '../components_shared/SiteNav';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.footer`
+  background: var(--pageHeaderColor);
+  -webkit-box-shadow: 0 8px 6px -6px black;
+  -moz-box-shadow: 0 8px 6px -6px black;
+  box-shadow: 0 -8px 6px -6px black;
+  position: relative;
+  bottom: 0;
+`;
+const InnerWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: space-around;
+  margin: 0 auto;
+`;
+const FooterText = styled.div`
+  font-size: 2rem;
+  width: 50%;
+  margin: 0 auto;
+  text-align: center;
+`;
 
-export default function Header() {
+export default function Footer() {
   return (
     <Wrapper>
-      <Link href="/">Footer</Link>
+      <FooterText>
+        <p>
+          Owl Tactics Crypto does not provide financial advice. The financial
+          opinions expressed are for information purposes only.&nbsp; Please
+          seek professional counsel for your financial imperatives. <br />
+        </p>
+      </FooterText>
+      <InnerWrapper>
+        <SocialsNav />
+        <SiteNav />
+      </InnerWrapper>
+      <FooterText>
+        <p>Updated May {new Date().getFullYear()}</p>
+      </FooterText>
     </Wrapper>
   );
 }
