9a03652a081548b0893e9bb42996bff9f8a1d15b
Tim H <timh1203@gmail.com>
2021-05-20 22:51:23 -0500

    Add fonts, font preloads, pictures

--- components_shared/Page.tsx
+++ components_shared/Page.tsx
@@ -12,7 +12,13 @@ const GlobalStyles = createGlobalStyle`
     --pageHeaderColor: #1c1f21;
     --pageContentColor: #252525;
     --pageFontColor: #eeeeee;
+    --lightBrown: #dcc2a8;
+    --darkBrown: #694d3c;
     --maxWidth: 1000px;
+    --yashi: 'Yashi', serif;
+    --comfortaaL: "Comfortaa-Light", sans-serif;
+    --comfortaaR: "Comfortaa-Regular", sans-serif;
+    --comfortaaB: "Comfortaa-Bold", sans-serif;
     box-sizing: border-box;
     font-size: 62.5%;
   }
@@ -20,10 +26,10 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: inherit;
   }
   body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: var(--comfortaaR);
     padding: 0;
     margin: 0;
-    font-size: 1.8rem;
+    font-size: 1.6rem;
     color: var(--pageFontColor);
     line-height:2;
     background: var(--pageContentColor);
@@ -31,11 +37,42 @@ const GlobalStyles = createGlobalStyle`
   h1 { font-size: 2.8rem; }
   h2 { font-size: 2.4rem; }
   h3 { font-size: 2rem; }
+  button {
+    font-family: var(--comfortaaR);
+  }
   a {
     color: var(---grey);
   }
   a:hover {
     text-decoration: underline;
+  }
+  @font-face {
+    font-family: "Yashi";
+    src: url("/fonts/Yashi/Yashi-Regular.otf");
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Comfortaa-Light";
+    src: url("/fonts/Comfortaa/Comfortaa-Light.ttf");
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Comfortaa-Regular";
+    src: url("/fonts/Comfortaa/Comfortaa-Regular.ttf");
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Comfortaa-Bold";
+    src: url("/fonts/Comfortaa/Comfortaa-Bold.ttf");
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
   }
 `;
 const Wrapper = styled.div`
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,31 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-US'>
         <Head>
-          <link rel='shortcut icon' href='/ot2_50.ico' />
+          <link rel='shortcut icon' href='images/ot2_50.ico' />
+          <link
+            rel='preload'
+            href='/fonts/Yashi/Yashi-Regular.otf'
+            as='font'
+            crossOrigin=''
+          />
+          <link
+            rel='preload'
+            href='/fonts/Comfortaa/Comfortaa-Light.ttf'
+            as='font'
+            crossOrigin=''
+          />
+          <link
+            rel='preload'
+            href='/fonts/Comfortaa/Comfortaa-Regular.ttf'
+            as='font'
+            crossOrigin=''
+          />
+          <link
+            rel='preload'
+            href='/fonts/Comfortaa/Comfortaa-Bold.ttf'
+            as='font'
+            crossOrigin=''
+          />
         </Head>
         <body>
           <Main />
