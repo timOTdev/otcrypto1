b69f018f494d5a3af1b62d688630b0320097d266
Tim H <19733413+timh1203@users.noreply.github.com>
2021-05-24 22:30:39 -0500

    Fix test data message

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,20 @@ import Banner from '../components/home/Banner';
 import Learn from '../components/home/Learn';
 import Recent from '../components/home/Recent';
 import Discord from '../components/home/Discord';
-import { data } from '../data/youtubeAPI';
+// import { data } from '../data/youtubeAPI';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const isTestEnvironment = process.env.IS_TEST_ENVIRONMENT;
-  console.log('isTestEnvironment', isTestEnvironment);
+  // const isTestEnvironment = process.env.IS_TEST_ENVIRONMENT;
+  // console.log('isTestEnvironment', isTestEnvironment);
 
-  if (isTestEnvironment) {
-    return { props: { data } };
-  } else {
-    const URL = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet&order=date&maxResults=3`;
-    const res = await fetch(URL);
-    const data = await res.json();
-    return { props: { data } };
-  }
+  // if (isTestEnvironment) {
+  //   return { props: { data } };
+  // } else {
+  const URL = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet&order=date&maxResults=3`;
+  const res = await fetch(URL);
+  const data = await res.json();
+  return { props: { data } };
+  // }
 };
 
 export default function home({ data }) {
