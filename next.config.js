ea0e8312443de3723939448a6e709eb3743df724
Tim H <timh1203@gmail.com>
2021-05-24 21:47:59 -0500

    Add youtube data file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
     YOUTUBE_CHANNEL_ID: process.env.YOUTUBE_CHANNEL_ID,
+    IS_TEST_ENVIRONMENT: process.env.IS_TEST_ENVIRONMENT,
   },
   future: {
     webpack5: true,
