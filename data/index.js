d53e27180ce67730d1c11f66f555d3adff0eac76
Tim H <timh1203@gmail.com>
2021-05-30 13:32:57 -0500

    Add deals page and data

--- data/index.js
+++ data/index.js
@@ -33,5 +33,45 @@ export default function globals() {
     },
   };
 
-  return { socials, videos };
+  const deals = [
+    {
+      name: 'Celsius Network',
+      description:
+        'My go-to platform for staking Bitcoin and Ethereum. The rates are 1-2% higher than Nexo but I really like the staking variety, ability to withdraw without limits (unlike Nexo), and top-notch security and asset protection.',
+      offer:
+        'Use the referral code 15442521ce when signing up and earn $40 in BTC with your first transfer of $400 or more.',
+      link: 'https://celsiusnetwork.app.link/15442521ce',
+    },
+    {
+      name: 'Gate.io',
+      description:
+        "Binance.com and Binance.US is not available to Texas, USA. This is the next best thing if you're looking for huge amounts of coin offerings and more technical trading beyond Coinbase Pro or Crypto.com. This is not recommended for the crypto initiate.",
+      offer: 'We share 40% in commission rewards. I get 20%, you get 20%.',
+      link: 'https://www.gate.io/signup/5403310',
+    },
+    {
+      name: 'Surfshark',
+      description:
+        'This great VPN service helps keeps my browsing more secure with many connection locations. The service is affordable compared to ExpressVPN and NordVPN.',
+      offer: 'We both get a free month of service when you sign up.',
+      link: 'https://surfshark.club/friend/qLtLHArX',
+    },
+    {
+      name: 'Crypto.com',
+      description:
+        'My current crypto card. This 2% crypto rewards card in CRO was less complicated than other cards and helps convert my fiat to crypto. If you get the Ruby card, you also get free Spotify ($14 value). I will update this if I come across a better card.',
+      offer:
+        'Sign up for Crypto.com Ruby card or above and we both get $25 USD in CRO.',
+      link: 'https://crypto.com/app/9zqwdpcces ',
+    },
+    {
+      name: 'Koinly',
+      description:
+        'Koinly takes the konfusion out of tracking crypto taxes. I really like their interface because it shows my cost basis. I was able to link many exchanges including Coinbase, Coinbase Pro, Kraken, Nexo, Celsius Network, Yoroi Wallet, Gate.io, and Crypto.com Wallet. Push the easy button!',
+      offer: 'Give friends $20 off on their first order and get $20 in return.',
+      link: 'https://koinly.io/?via=BC293767',
+    },
+  ];
+
+  return { socials, videos, deals };
 }
--- pages/deals.tsx
+++ pages/deals.tsx
@@ -1,6 +1,84 @@
-// @ts-ignore
-import Construction from '../components_shared/Construction.tsx';
+import styled from 'styled-components';
+import globals from '../data/index';
+
+const Main = styled.main`
+  width: 60%;
+  margin: 3rem auto;
+`;
+const Deals = styled.section`
+  width: 60%;
+  margin: 0 auto;
+  text-align: left;
+`;
+const Title = styled.a`
+  color: var(--desert);
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: underline;
+  }
+  &:hover {
+    color: var(--almond);
+    text-shadow: 0 0 10px var(--desert);
+  }
+`;
+const Offer = styled.span`
+  font-style: italic;
+`;
+
+const Expand = styled.div`
+  text-align: center;
+  a {
+    color: var(--desert);
+    &:focus,
+    &:hover,
+    &:visited,
+    &:link,
+    &:active {
+      text-decoration: none;
+    }
+    &:hover {
+      background: var(--almond);
+      color: var(--almond);
+      text-shadow: 0 0 10px var(--desert);
+    }
+  }
+`;
 
 export default function deals() {
-  return <Construction page={'Deals'} />;
+  const { deals } = globals();
+  return (
+    <Main>
+      <h1>Deals</h1>
+      <p>
+        The following links provided are affiliate links.
+        <br />
+        They help support the brand without any cost to you if you decide to
+        participate.
+        <br />
+        These services are ones I have personally used and recommend.
+        <br />
+        Please reach out to me on discord if you want a demo or have questions.
+      </p>
+      {deals.map((item, index) => (
+        <Deals key={item.name}>
+          <h2>
+            <Title href={item.link}>{item.name}</Title>{' '}
+          </h2>
+          <p key={item.description}>
+            <Offer>{item.offer}</Offer>
+          </p>
+          <p>{item.description}</p>
+          <Expand>
+            <a href={item.link} target='_blank'>
+              <button>Get {item.name} Deal</button>
+            </a>
+          </Expand>
+          {index == deals.length - 1 ? '' : <hr />}
+        </Deals>
+      ))}
+    </Main>
+  );
 }
